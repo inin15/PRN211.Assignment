d55de785e17162f465097c41d2850de74b8d0e56
inin15 <101159760+inin15@users.noreply.github.com>
2022-07-21 10:06:01 +0700

    Login
    Login

---
+++ Quanlyquancafe/Quanlyquancafe/Login.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Quanlyquancafe
+{
+    public partial class frmLogin : Form
+    {
+        public frmLogin()
+        {
+            InitializeComponent();
+        }
+        //Hàm buttom Exit
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            DialogResult ms = MessageBox.Show("Bạn có muốn thoát không? ", "Xác nhận", MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
+            if (ms == DialogResult.OK)
+            {
+                Application.Exit();
+            }
+        }
+        //Ham kiem tra Dang nhap
+        private bool CheckLogin(string username, string password, string typeA)
+        {
+            DataProvider provider = new DataProvider();
+            DataTable table = provider.loadAccount();
+            for (int i = 0; i < table.Rows.Count; i++)
+            {
+                if (table.Rows[i][0].ToString() == username && table.Rows[i][2].ToString() == password && table.Rows[i][3].ToString() == typeA)
+                {
+                    name = table.Rows[i][1].ToString();
+                    type = table.Rows[i][3].ToString();
+                    MessageBox.Show("Xin chào " + name + " :)", "Đăng nhập thành công", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    return true;
+                }
+            }
+            return false;
+        }
+
+    }
+}
